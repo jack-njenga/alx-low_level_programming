deae5fe6a52a7d1d388053f58c8303e57b477fc7
jack-njenga <jacknjenga99@gmail.com>
2023-02-16 11:08:52 -0800

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <stdio.h>
+int main() {
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long: %ld byte(s)\n", sizeof(long));
+    return (0);
+}
