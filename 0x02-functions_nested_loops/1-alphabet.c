df4854ec87c442964d2249d1df6defe81cf3d270
jack-njenga <jacknjenga99@gmail.com>
2023-02-21 08:17:11 -0800

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - print all lowercase alphabets
- * Return 0
+ * Return:void
  */
 
 void print_alphabet(void)
@@ -12,7 +12,6 @@ void print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
-		_putchar('\n');
 	}
-	
+	_putchar('\n');			
 }
