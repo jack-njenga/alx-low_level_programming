ab81db8a0d021140bd0fccd6c6e5d6b4df9c3073
jack-njenga <jacknjenga99@gmail.com>
2023-02-21 07:34:30 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return 0
+ */
+
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
