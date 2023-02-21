b64bc10d5ef0c2a54002917330677c96137a5b56
jack-njenga <jacknjenga99@gmail.com>
2023-02-21 12:28:12 -0800

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - prints the sign of the number
  * @n: the number to be cheched
  *
- * return:1 and print'+' if 'n' is positive, -1 & prints '-' if 'n' if negative, else 0  & prints 0
+ * Return: 1 for positive num, -1 for negative num 0r 0 for anything else
  */
 
 int print_sign(int n)
