29c2dc85b3a13c5b17f5ee874c9934aa32e3e062
jack-njenga <jacknjenga99@gmail.com>
2023-02-23 13:23:47 -0800

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - fizzbuzz
+ *
+ * Return: returns void
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	putchar(1 + '0');
+
+	for (i = 2; i <= 100; i++)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && !(i % 3 == 0))
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf('\n');
+}
+
+/**
+ * main - entry point
+ * Return: returns void
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
