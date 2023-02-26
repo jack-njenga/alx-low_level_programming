2c2e650ab74a149fbc3d0b269af05bb172fe5601
jack-njenga <jacknjenga99@gmail.com>
2023-02-26 14:31:24 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char text[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, text, 59);
 
