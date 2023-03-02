ad76a0b27f9a35a2a119080abde6cfa1c2b1da8a
jack-njenga <jacknjenga99@gmail.com>
2023-03-02 10:18:18 -0800

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,14 @@
  *
  * Return:(char)
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && src[i] !=  '\0'; i++)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	while (i < n)
 	{
