ab628f6d96eb746343d266d906aa6a3bffc30163
jack-njenga <jacknjenga99@gmail.com>
2023-03-06 13:45:13 -0800

    0x07

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,12 @@
 #include "main.h"
+#include <unistd.h>
 /**
+ * _putchar - write character to the stdout
+ * @c: character to write
+ * Return: 1(success) else (on error)
  */
 
-int _putchar()
+int _putchar(char c)
 {
-	
+	return (write(1, &c, 1));
 }
