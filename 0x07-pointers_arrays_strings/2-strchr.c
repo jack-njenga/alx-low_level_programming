beac0d6c6e71b4f0562215fbc5d1e0b5b6edb74c
jack-njenga <jacknjenga99@gmail.com>
2023-03-07 00:11:40 +0300

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - locates a character in astring
+ * @s:string
+ * @c: character to search
+ * Return: pointer to first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (1)
+	{
+		i = *s++;
+		if (i == c)
+		{
+			return (s - 1);
+		}
+		if (i == 0)
+		{
+			return (NULL);
+		}
+	}
+}
