6bb64910ddf1a763ae0655337be1c695457bc879
jack-njenga <jacknjenga99@gmail.com>
2023-03-09 00:49:54 -0800

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _is_prime_number - checks if an integer is prime
+ * is_prime_number - checks if an integer is prime
  * @n:number to check
  * Return:1 os 0
  */
@@ -10,7 +10,7 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	if (n > 2 && n % 2 == 0 || n > 3 && n % 3 == 0)
+	if ((n > 2 && n % 2 == 0) || (n > 3 && n % 3 == 0))
 	{
 		return (0);
 	}
