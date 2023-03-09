70bc485a33a407849ef63df4410e3818e1b0c596
jack-njenga <jacknjenga99@gmail.com>
2023-03-09 22:36:39 +0300

    0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,37 @@
 #include "main.h"
+
+int prime_helper(int n, int i);
 /**
  * is_prime_number - checks if an integer is prime
- * @n:number to check
+ * @n: number to check
  * Return:1 os 0
  */
 int is_prime_number(int n)
+{
+	int i = 2;
+
+	return (prime_helper(n, i));
+}
+/**
+ * prime_helper - helps is_prime_number function
+ * @n: number to check
+ * @i: recurvisive integer
+ * Return: 1 or 0
+ */
+int prime_helper(int n, int i)
 {
 	if (n < 2)
 	{
 		return (0);
 	}
-	if ((n > 2 && n % 2 == 0) || (n > 3 && n % 3 == 0))
+	if (n % i == 0 && i < n)
 	{
 		return (0);
 	}
+	if (i == n)
+	{
+		return (1);
+	}
 	else
-	return (1);
+	return (prime_helper(n, i + 1));
 }
