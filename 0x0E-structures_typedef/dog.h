ff1e03f1b29488147311ea62427b74bebf592b3d
jack-njenga <jacknjenga99@gmail.com>
2023-03-20 22:26:55 +0300

    0x0D-preprocessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the structr of the dog
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner: the dogs master
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
