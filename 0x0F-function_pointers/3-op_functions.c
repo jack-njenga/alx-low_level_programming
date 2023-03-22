c21a60ed8e80bbfb8d3a98c75bca9d45c2af9253
jack-njenga <jacknjenga99@gmail.com>
2023-03-22 22:48:32 +0300

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adds
+ * @a: num1
+ * @b: num2
+ * Return: ans
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - minus
+ * @a: num1
+ * @b: num2
+ * Return: ans
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a :num1
+ * @b: num2
+ * Return: ans
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: num1
+ * @b: num2
+ * Return: ans
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder
+ * @a: num1
+ * @b: num2
+ * Return: ans
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
