f2c9597840bd5f845e768c96109cb84e1e739050
jack-njenga <jacknjenga99@gmail.com>
2023-03-22 18:40:12 +0300

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,22 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-	{
-		return (0);
-	}
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
 	for (i = 0; i != size; i++)
 	{
-		if (cmp(array[i]) == 1)
-		break;
+		if (cmp(array[i]) != 0)
+		return (i);
 	}
-	if (i == size)
-	{
-		return (0);
-	}
-	return (i);
+	return (0);
 }
