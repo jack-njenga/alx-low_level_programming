a9167d3dd014f315de586851e7d34ad1247f6166
jack-njenga <jacknjenga99@gmail.com>
2023-04-04 12:47:11 +0300

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,35 +9,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *n_node, *tmp, *temp;
+	unsigned int i = 0;
+	listint_t *n_node, *temp;
 
 	n_node = malloc(sizeof(listint_t));
-	if (n_node == NULL || head == NULL)
+	if (n_node == NULL || *head == NULL)
+		return (NULL);
+
+	n_node->n = n;
+	n_node->next = NULL;
+
+	if (idx == 0)
 	{
+		n_node->next = *head;
+		*head = n_node;
+		return (n_node);
+	}
+
+	temp = *head;
+	while (temp != NULL && i < (idx - 1))
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (i != (idx - 1))
+	{
+		free(n_node);
 		return (NULL);
 	}
 
-	n_node->n = n;
-	tmp = *head;
-	while (tmp != NULL)
-	{
-		if (idx == 0)
-		{
-			temp = tmp->next;
-			tmp->next = n_node;
-			n_node->next = temp;
-			return (n_node);
-		}
-		if (i == idx)
-		{
-			temp = tmp->next;
-			tmp->next = n_node;
-			n_node->next = temp;
-			return (n_node);
-		}
-		tmp = tmp->next;
-		i++;
-	}
-	return (NULL);
+	n_node->next = temp->next;
+	temp->next = n_node;
+
+	return (n_node);
 }
