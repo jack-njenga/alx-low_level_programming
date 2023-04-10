55fffebeff004c3b78f77cafe630fe5ae3f8ba0c
jack-njenga <jacknjenga99@gmail.com>
2023-04-10 21:59:26 +0300

    update file_I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,29 @@
 #include "main.h"
 /**
- * creat_file - creates a file 
+ * create_file - creates a file
  * @filename: name of the file
  * @text_content: the content to write to the file
  * Return: 1 on success -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr;
+	int fd, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (access(filename, F_OK) != -1)
-	{
-		fd = open(filename, O_WRONLY | O_TRUNC);
-		if (fd == -1)
-			return (-1);
-		wr = write(fd, text_content, strlen(text_content));
-		if (wr == -1)
-			return (-1);
-		return (1);
-		close(fd);
 
-	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		fd = open("filename", O_CREAT, 0600);
-		if (fd == -1)
-			return (-1);
-
-		close (fd);
-		return (1);	
+		for (len = 0; text_content[len]; len++);
 	}
-	fd = open("filename", O_WRONLY | O_CREAT, 0600);
-	if (fd == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(fd, text_content, len);
+
+	if (fd == -1 || wr == -1)
 		return (-1);
-	wr = write(fd, text_content, strlen(text_content));
-	if (wr == -1)
-		return (-1);
+
 	close(fd);
 
-	return (1);
+	return (-1);
 }
