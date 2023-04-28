9c6acfbb2420974220b5ea28b12c558161b68ffb
jack-njenga <jacknjenga99@gmail.com>
2023-04-28 11:45:03 -0700

    update on elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,15 @@ void close_file(int fd);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 #endif
