5e16f1b3734c940beb9cb5fee0b9d7dd9dcf09e4
jack-njenga <jacknjenga99@gmail.com>
2023-05-23 00:54:31 +0300

    Code injection: Win the Giga Millions!

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: EXIT SUCCeSS.
+ */
+
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
+
---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export gm="echo ./gm 9 8 10 24 75 9\n-->Please Make me win"
