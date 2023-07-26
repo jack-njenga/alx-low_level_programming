be9bc0616ab84da52b2b91f6da1a67e04eacac7f
jack-njenga <jacknjenga99@gmail.com>
2023-07-26 18:39:26 +0300

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,15 +10,16 @@ def island_perimeter(grid):
     """
     height = len(grid)
     edg = 0
+    sz = 0
 
     for h in range(height):
         width = len(grid[h])
         for w in range(width):
             if grid[h][w] == 1:
-                size = size + 1
+                sz = sz + 1
                 if w > 0 and grid[h][w - 1] == 1:
                     edg += 1
-                id h > 0 and grid[h - 1][w] == 1:
+                if h > 0 and grid[h - 1][w] == 1:
                     edg += 1
-    ans = size * 4 - (edg * 2)
+    ans = sz * 4 - (edg * 2)
     return ans
