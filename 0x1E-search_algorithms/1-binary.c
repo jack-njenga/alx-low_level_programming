838b82abab061a0a0fb920121ca34a72917388a3
jack-njenga <jacknjenga99@gmail.com>
2023-09-13 16:12:22 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * binary_search - Searches for a value in a sorted array
  * of integers using the Binary search algorithm
@@ -8,63 +7,54 @@
  * @size: The number of elements in the array.
  * @value: The value to search.
  *
- * Return: Returns the index where the value is at on success, Otherwise -1 on failure.
+ * Return: Returns the index where the value is at
+ * on success, Otherwise -1 on failure.
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	int *arr = array, sz = (size / 2);
+	int left = 0, right, mid;
 
-	if ((!array) || size == 0)
+	if (!array || size == 0)
 		return (-1);
-	else
+	right = size - 1;
+
+	while (left <= right)
 	{
-		while (value != sz)
-		{
-			printf("sz = %d\n", sz);
-			if (value < sz)
-			{
-				printf("%d is < %d -- %ld\n", value, sz, sizeof(arr));
-				arr = divide_array(arr, sz, -1);
-			}
-			else if (value > sz)
-			{
-				printf("%d is > %d -- %ld\n", value, sz, sizeof(arr));
-				arr = divide_array(arr, sz, 1);
-			}
-			else
-			{
-				printf("%d is == %d --%ld\n", value, sz, sizeof(arr));
-				free(arr);
-				return (sz);
-			}
-			sz = (size / 2);
-			if (sizeof(arr) < 8)
-				return (-1);
-		}
+		print_array(array, left, right);
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
 	return (-1);
 }
 
-int *divide_array(int *array, int sz, int i)
+/**
+ * print_array - prints the current array.
+ *
+ * @arr: Array to print
+ * @start: Where to start printing from.
+ * @end: As the name suggests where to stop printing at.
+ *
+ * Return: This one shall return void.
+ */
+void print_array(int *arr, int start, int end)
 {
-	int *new_arr = malloc(sz * sizeof(int));
-	int n;
-	
-	if (i == -1)
+	int i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
 	{
-		for (n = 0; n < sz; n++)
-		{
-			new_arr[n] = array[n];
-		}
+		if (i == end)
+			printf("%d", arr[i]);
+		else
+			printf("%d, ", arr[i]);
 	}
-	else
-	{
-		for (n = 0; array[sz] != '\0'; n++)
-		{
-			new_arr[n] = array[sz];
-			sz++;
-		}
-	}
-	return (new_arr);
+	printf("\n");
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,6 @@
 /* prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int *divide_array(int *array, int sz, int i);
+void print_array(int *arr, int start, int end);
 
 #endif
